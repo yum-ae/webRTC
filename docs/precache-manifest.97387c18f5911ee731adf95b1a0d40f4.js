5c7e7471754636fd0aa7ef2f1861877072ec0706
Your Name <you@example.com>
2020-07-26 12:42:32 +0000

    docs added

---
+++ docs/precache-manifest.97387c18f5911ee731adf95b1a0d40f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc7c2552d23d6c94fbd",
+    "url": "/css/app.2b93f8bf.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2396da059c964dc7a061acf11a8ac007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab309ec73e25c92eff32",
+    "url": "/js/about.c7194cba.js"
+  },
+  {
+    "revision": "edc7c2552d23d6c94fbd",
+    "url": "/js/app.f5876cc5.js"
+  },
+  {
+    "revision": "11c7993b8205cc0a342a",
+    "url": "/js/chunk-vendors.c931d42d.js"
+  },
+  {
+    "revision": "8ee3b88d55067381c04a7a6fe9995b42",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
